8e3d4c740a0e94042bb3a13faf0bf7c370d45f75
Jack <29169102+Jack5079@users.noreply.github.com>
2020-07-01 14:06:26 -0400

    improvements

--- keyboard.js
+++ keyboard.js
@@ -1,35 +1,43 @@
-const keys = `\`1234567890-=\!@#$%^&*()_+|qwertyuiop[]QWERTYUIOP{}asdfghjkl;'ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>?`.split('')
+const keys = Array(5000).fill(0).map((_, i) => String.fromCharCode(i)).splice(33)
+//`\`1234567890-=\!@#$%^&*()_+|qwertyuiop[]QWERTYUIOP{}asdfghjkl;'ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>?`.split('')
 const keycont = document.getElementById('keys')
 
-/**
- * @param {string | any[]} arr
- */
-const random = arr => arr[Math.floor(Math.random() * arr.length)]
-const randomPercent = () => random([...Array(100).keys()]) + '%'
 
 const align = ['left', 'right', 'center']
-function randomColor () {
-  var letters = '0123456789ABCDEF'.split('')
-  var color = '#'
-  for (var i = 0; i < 6; i++) color += random(letters)
-  return color
-}
 
+const random = {
+  /**
+   * @param {string | any[]} arr
+   */
+  fromArray (arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+  },
+
+  get percent () {
+    return this.fromArray([...Array(100).keys()]) + '%'
+  },
+  get color () {
+    var letters = '0123456789ABCDEF'.split('')
+    var color = '#'
+    for (var i = 0; i < 6; i++) color += this.fromArray(letters)
+    return color
+  }
+}
 function shuffle () {
   keycont.innerHTML = ''
 
-  keycont.style.textAlign = random(align)
-    ;[...keys, '<a href=".">↻</a>'].sort(() => Math.random() - 0.5).forEach(string => {
-      const key = document.createElement('span')
-      key.style.background = Math.random() > 0.5 ? `linear-gradient(to top right, ${randomColor()}, ${randomColor()})` : randomColor()
-      key.style.textAlign = random(align)
-      key.style.color = randomColor()
-      key.style.borderRadius = randomPercent()
-      key.style.width = randomPercent()
-      key.style.height = randomPercent()
-      key.innerHTML = string
-      keycont.appendChild(key)
-    })
+  keycont.style.textAlign = random.fromArray(align)
+  keys.sort(() => Math.random() - 0.5).forEach(string => {
+    const key = document.createElement('span')
+    key.style.background = Math.random() > 0.5 ? `linear-gradient(to top right, ${random.color}, ${random.color})` : random.color
+    key.style.textAlign = random.fromArray(align)
+    key.style.color = random.color
+    key.style.borderRadius = random.percent
+    key.style.width = random.percent
+    key.style.height = random.percent
+    key.innerHTML = string
+    keycont.appendChild(key)
+  })
 
 }
 const inp = document.querySelector('input')
